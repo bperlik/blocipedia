6f4d1ddfa8ff4eec3cd352037c26333f7ef9c227
Barbara Perlik <barbperlik@gmail.com>
2017-06-03 21:14:18 -0400

    Show wiki & create new wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,11 +4,30 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+    @wiki.user = current_user
+    if @wiki.save
+      flash[:notice] = "Wiki '#{@wiki.title}' was saved."
+      redirect_to [@wiki]
+    else
+      flash.now[:alert] = "There was an error saving the wiki.  Please try again."
+      render :new
+    end
   end
 
   def edit
   end
+
+  def wiki_params
+    params.require(:wiki).permit(:title,:body, :private)
+  end
+
 end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe WikisController, type: :controller do
 
   let(:my_wiki) { Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
@@ -23,16 +22,43 @@ RSpec.describe WikisController, type: :controller do
 #    end
 #  end
 
-#  describe "GET #new" do
-#    it "returns http success" do
-#      get :new
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+
+   describe "POST create" do
+
+     it "increases the number of Wiki by 1" do
+       expect{wiki :create, wiki:{title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Wiki, :count).by(1)
+     end
+
+     it "assigns the new wiki to @wiki" do
+       post :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+       expect(assigns(:wiki)).to eq Wiki.last
+     end
+
+     it "redirects to the new wiki" do
+        post :create, wiki: {title: RandomData.randomw_sentence, body: RandomData.random_paragraph}
+        expect(response).to redirect_to Wiki.last
+     end
+   end
 
 #  describe "GET #edit" do
 #    it "returns http success" do
-#      get :edit
+#      get :edi
 #      expect(response).to have_http_status(:success)
 #    end
 #  end
